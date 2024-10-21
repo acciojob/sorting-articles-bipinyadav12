0ee240235967d66c970c3a34fbc02e5877256c44
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-10-21 22:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
- bands.sort();
+  function stripArticle(bands) {
+            return bands.replace(/^(a|an|the)\s+/i, '').toLowerCase();
+        }
+
+        
+        const sortedArticles = bands.sort((a, b) => {
+            return stripArticle(a).localeCompare(stripArticle(b));
+        });
