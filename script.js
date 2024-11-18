f3bf8b58ebdcf96f5f0693209ae028a1d1f2012a
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-11-18 10:33:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The https://www.svgrepo.com/show/345221/three-dots.svgDevil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
  function stripArticle(title) {
             return title.replace(/^(a |an |the )/i, '').trim();
         }
 
-       const ulElement = document.getElementById('band');
+       const ulElement = document.getElementById('bands');
         const itemsArray = Array.from(ulElement.getElementsByTagName('li'));
 
-        // Sort the items while ignoring articles
         itemsArray.sort((a, b) => stripArticle(a.textContent).localeCompare(stripArticle(b.textContent)));
 
         // Clear the original list and append the sorted items
