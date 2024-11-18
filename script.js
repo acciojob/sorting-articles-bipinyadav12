12ce1225c1ed40578b55bd0884d2950361d8ab1e
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-11-18 10:43:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
  function stripArticle(title) {
             return title.replace(/^(a |an |the )/i, '').trim();
-        }
-
+       
        const ulElement = document.getElementById('bands');
         const itemsArray = Array.from(ulElement.getElementsByTagName('li'));
 
         itemsArray.sort((a, b) => stripArticle(a.textContent).localeCompare(stripArticle(b.textContent)));
 
-        // Clear the original list and append the sorted items
+     alert(itemsArray);
         ulElement.innerHTML = '';
         itemsArray.forEach(item => ulElement.appendChild(item));
 
 
         
-       
+    
